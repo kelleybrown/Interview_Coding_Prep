95ca03ef4c82f06a364b81b3acdcaa67da74fe8a
Matriks50 <ahmetsalesforce@gmail.com>
2024-02-02 08:56:17 -0500

    BinaryRepresentationOfN created

---
+++ src/Ahmet_Q2_BinaryRepresentationOfN.java
@@ -0,0 +1,47 @@
+public class Ahmet_Q2_BinaryRepresentationOfN {
+
+    public static void main(String[] args){
+        System.out.println(binaryOfN(10));
+        System.out.println(binaryOfN(15));
+        System.out.println(binaryOfN(16));
+
+    }
+    public static int binaryOfN(int n){ // 6 -> 3 -> 1 -> 0
+
+        String binary = ""; // 0 -> 10 -> 110
+
+        while(n > 0){
+            binary = n % 2 + binary;
+            n /= 2;
+        }
+
+        return binary.replace("0", "").length();
+    }
+
+
+}
+
+ /*
+        Binary Representation Of N
+
+        Create a method to determine how many 1s are in the representation of the number in binary form
+            parameter: int
+            return: int
+
+            The given n will be more than 0
+
+        Ex:
+            input: 6
+            output: 2
+
+            6 in binary is 110
+            there are two '1' digits, so the output is 2
+
+        Test data:
+            6 -> 110 -> 2
+            7 -> 111 -> 3
+            4 -> 100 -> 1
+            10 -> 1010 -> 2
+            15 -> 1111 -> 4
+            16 -> 10000 -> 1
+     */
